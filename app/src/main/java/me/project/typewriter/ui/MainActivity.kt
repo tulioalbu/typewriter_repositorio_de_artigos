2e9344829349b333844b53a1df938516d9a91581
tulioalbu <tulioalbu@gmail.com>
2022-10-25 18:18:48 -0300

    criação do layout

---
+++ app/src/main/java/me/project/typewriter/ui/MainActivity.kt
@@ -0,0 +1,12 @@
+package me.project.typewriter.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import me.project.typewriter.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
---
+++ app/src/main/java/me/project/typewriter/ui/SplashScreenActivity.kt
@@ -0,0 +1,27 @@
+package me.project.typewriter.ui
+
+import android.annotation.SuppressLint
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import androidx.appcompat.app.AppCompatActivity
+import me.project.typewriter.R
+
+
+@SuppressLint("CustomSplashScreen")
+class SplashScreenActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash_screen)
+
+        supportActionBar?.hide()
+
+        Handler(Looper.getMainLooper()).postDelayed({
+            val intent = Intent(this@SplashScreenActivity, MainActivity::class.java)
+            startActivity(intent)
+        }, 2000)
+    }
+}
+
+
